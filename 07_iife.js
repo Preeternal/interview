5db0c4450c3869b51fdf739974f1bedb64987b39
Preeternal <preeternal@gmail.com>
2022-06-06 12:55:34 +0300

    07

---
+++ 07_iife.js
@@ -0,0 +1,17 @@
+//immediately invoked function expression
+let result = []
+for (var i=0; i<5; i++) {
+  (
+    function() {
+      var j = i;
+      result.push(
+        function() {
+          console.log(j);
+        }
+      )
+    }
+  )()
+  
+}
+
+result[2]();
